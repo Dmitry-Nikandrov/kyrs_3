c7a1a09688dddbf6ec4befef2b8672f6cc3ed684
Dmitry <dnikandrov@yandex.ru>
2024-12-10 19:40:46 +0300

    the end

--- src/views.py
+++ src/views.py
@@ -6,25 +6,29 @@ import pandas as pd
 from src.utils import common_information, get_excel_df, get_exchange_rate, get_stock_price, greetings, top_5_operations
 
 
-def main(str_time):
+def main(str_time: object) -> object:
     """принимает дату в формате строки YYYY-MM-DD HH:MM:SS и возвращает общую информацию в формате
     json о банковских транзакциях за период с начала месяца до этой даты"""
-    data = get_excel_df("operations.xlsx")
-    data_df = pd.DataFrame(data)
-    date_obj = datetime.strptime(str_time, "%Y-%m-%d %H:%M:%S")
-    data_df["datetime"] = pd.to_datetime(data_df["Дата операции"], dayfirst=True)
-    json_data = data_df[
-        (data_df["datetime"] >= (date_obj - timedelta(days=date_obj.day - 1))) & (data_df["datetime"] <= date_obj)
-    ]
+    try:
+        data = get_excel_df("operations.xlsx")
+        data_df = pd.DataFrame(data)
+        date_obj = datetime.strptime(str_time, "%Y-%m-%d %H:%M:%S")
+        data_df["datetime"] = pd.to_datetime(data_df["Дата операции"], dayfirst=True)
+        json_data = data_df[
+            (data_df["datetime"] >= (date_obj - timedelta(days=date_obj.day - 1))) & (data_df["datetime"] <= date_obj)
+        ]
 
-    agg_dict = {
-        "greetings": greetings(),
-        "cards": common_information(json_data),
-        "top_transactions": top_5_operations(json_data),
-        "currency_rates": get_stock_price("user_settings.json"),
-        "stock_prices": get_exchange_rate("user_settings.json"),
-    }
-    return json.dumps(agg_dict, ensure_ascii=False)
+        agg_dict = {
+            "greetings": greetings(),
+            "cards": common_information(json_data),
+            "top_transactions": top_5_operations(json_data),
+            "currency_rates": get_stock_price("user_settings.json"),
+            "stock_prices": get_exchange_rate("user_settings.json"),
+        }
+        return json.dumps(agg_dict, ensure_ascii=False)
+    except Exception as e:
+        print(f"!!!! Неудачное формирование отчета. Ошибка - {e}")
+        return []
 
 
 print(main(str_time="2020-08-11 15:25:00"))
